44236c7fcbef9c687827fa67779b3c9202951ffd
StackCat <dbsxo1234@gmail.com>
2017-06-30 13:30:33 +0900

    V1.01 배송비 추가, 자잘한 오류 및 기능 수정

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,6 @@
 Naver Shopping Crawler
 
-V1.0
+V1.10
 단순 키워드 입력시 해당 키워드를 검색어로, Naver Shopping페이지에 검색 후 나온 데이터를 수집. 
 
 한 페이지당 80개 출력 기준으로 10개 페이지 수집
@@ -9,6 +9,13 @@ V1.0
 
 JSON 파일 양식
 {
-    "size" : 000,
-    "result" : [...]
+    "size" : Int,
+    "result" : [{ "name":String, "price":StringInt, "seller":String, "DeliveryPrice":value}...]
 }
+
+2017. 06. 30. - V 1.1
+이름, 가격, 판매자, 배송비 수집 및 출력
+자잘한 오류 및 기능 수정
+
+2017. 06. 30. - V 1.0
+이름, 가격, 판매자 수집 및 출력
--- crawling.py
+++ crawling.py
@@ -6,11 +6,11 @@ import json
 import urllib
 import random
 from time import sleep
-
+import testHTMLsrc
 """
 2017. 06. 29 Developed by KYT
 NaverShopping Crawler
-V 1.0
+V 1.10
 
 
 class info--
@@ -36,7 +36,10 @@ def getPrice(goods):
     try:
         price = (goods.find_all("span", class_="num _price_reload"))[0].text
     except:
-        price = (goods.find_all("span", class_="num"))[0].text
+        try:
+            price = (goods.find_all("span", class_="num"))[0].text
+        except:
+            print("\n\nerror on getPrice\n\n\n{}\n\n\nerror\n\n".format(goods))
     return price
 
 def getSeller(goods):
@@ -45,17 +48,40 @@ def getSeller(goods):
         seller = (goods.find_all("a", class_="mall_img"))[0].contents[0]
         seller = preProcessing(seller)
     except:
-        sellertest = goods.find_all("p", class_="mall_txt")
-        seller = (goods.find_all("p", class_="mall_txt"))[0].contents[1].text
-        seller = preProcessing(seller)
+        try:
+            sellertest = goods.find_all("p", class_="mall_txt")
+            seller = (goods.find_all("p", class_="mall_txt"))[0].contents[1].text
+            seller = preProcessing(seller)
 
-        if seller == "":
-            seller = sellertest[0].contents[1].contents[0].attrs["alt"]
+            if seller == "":
+                seller = sellertest[0].contents[1].contents[0].attrs["alt"]
+        except:
+            print("\n\nerror on getSeller\n\n\n{}\n\n\nerror\n\n".format(goods))
     return seller
+
+def getDeliveryPrice(goods):
+    # DPrice have three kind  (someInt, free, null)
+    try:
+        DPriceTest = (goods.find_all("ul", class_="mall_option"))
+        DPrice = (goods.find_all("ul", class_="mall_option"))[0].contents[1].text
+        if "네이버페이포인트" in DPrice:
+            DPrice = (goods.find_all("ul", class_="mall_option"))[0].contents[3].text
+
+        DPrice = preProcessing(DPrice)
+        DPrice = DPrice.replace("배송비 ","")
+        DPrice = DPrice.replace("원", "")
+    except:
+        try:
+            if len(DPriceTest) == 0 :
+                DPrice = "정보없음"
+        except:
+            print("\n\nerror on getDeliveryPrice\n\n\n{}\n\n\nerror\n\n".format(goods))
+
+    return DPrice
 
 def main():
     print("""
-    Naver Shopping Crawler (V1.0)
+    Naver Shopping Crawler (V1.10)
     Developed by StackCat
     """)
     while True:
@@ -69,6 +95,7 @@ def main():
         with open(fnm + '.json', 'w', encoding='utf8') as f:
             result = []
             for pagingIndex in range(1,11):
+
                 # ban 방지용
                 waitTime = random.randint(3,7)
                 print("keyword:"+fnm+" | pagingIndex : "+str(pagingIndex) + " | wait : "+str(waitTime))
@@ -82,21 +109,19 @@ def main():
                 for ultag in goods_list_result:
                     for goods in ultag.contents:
                         if goods == "\n" : continue
-                        try:
-                            name = getName(goods)
-                            price = getPrice(goods)
-                            seller = getSeller(goods)
-
-                        except:
-                                print("\n\nerror\n\n\n{}\n\n\nerror\n\n".format(goods))
-                                continue
-                        data = { 'name':parse.unquote(name), 'price':parse.unquote(price), 'seller':parse.unquote(seller) }
+                        name = getName(goods)
+                        price = getPrice(goods)
+                        seller = getSeller(goods)
+                        DPrice = getDeliveryPrice(goods)
+                        data = { 'name':parse.unquote(name), 'price':parse.unquote(price), 'seller':parse.unquote(seller), 'DeliveryPrice':parse.unquote(DPrice) }
                         result.append(data)
                 if len(result) == 0 :
                     print("검색결과가 없습니다.")
                     break
+
                 # ban 방지용
                 sleep(waitTime)
+
             finalData = { 'size' : len(result), 'result': result }
             json.dump(finalData,f, ensure_ascii=False)
 
